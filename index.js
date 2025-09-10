22656997965e2274217fddbfcffa76f498f519db
zbananasx <timurgasakim@gmail.com>
2025-09-10 04:08:32 +0300

    first commit

--- index.js
+++ index.js
@@ -5,8 +5,16 @@ const bot = new Telegraf(token.telegramToken, {});
 
 // Обрабатываем команду /start
 bot.start((ctx) => {
-    ctx.reply(`Привет хуесос, ой, то есть ${ctx.from.first_name}, короче можешь написать команду /help и вся хуйня выйдет
-        `);
+    ctx.reply(`Привет хуесос, ой, то есть ${ctx.from.first_name}, короче вот тебе кнопочки жамкай`, {
+        reply_markup: {
+            inline_keyboard: [
+                [{text: 'тапалка ебейшая228', web_app: {url: 'https://web-app-production-536a.up.railway.app/'}}],
+                [{text: 'чемпион жесткий', url: 'https://steamcommunity.com/profiles/76561199008763629/'}],
+                [{text: 'нуб нубовый', url: 'https://steamcommunity.com/id/IA_NE_PENDOS'}]
+            ]
+        }
+    }
+        );
 });
 setMenu(bot)
 help(bot)
